37682f2aae605e4720397a8ea5ec8cde888b68b4
Revlin John <stylogicalmaps@gmail.com>
2014-08-04 09:58:10 -0700

    add person list

--- Practice-AngularJS.aspx
+++ Practice-AngularJS.aspx
@@ -5,28 +5,28 @@
     <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 
     <link rel="stylesheet" href="jquery/ui/themes/smoothness/jquery-ui.css" />
-	<link rel="stylesheet" href="styles/practice.css" />
-	
+    <link rel="stylesheet" href="styles/practice.css" />
+
     <script type="text/javascript" src="jquery/jquery.min.js"></script>
     <script type="text/javascript" src="jquery/jquery.mobile.min.js"></script>
     <script type="text/javascript" src="angularjs/angular.min.js"></script>
 </head>
 <body data-role="page" 
-	  data-ng-app="practice-angularjs"
+      data-ng-app="practice-angularjs"
       data-ng-init="name='Mundo'">
 
     <form id="form1" runat="server">
 
         <h1 data-ng-controller="HelloController">{{greet(name)}}</h1>
         <div data-ng-controller='typeBoxController' style="max-width:240px;">
-		    <h2 data-role="header" style="text-overflow:ellipsis; overflow:hidden; ">{{greeting}} "{{usrmsg}}"</h2>
-			    You: <div class="practice-user-message"></div>
-		    <p>
-			    <span data-ng-show="remaining()">Keep Typing</span><span data-ng-show="(! remaining() )">Stop Typing</span> (
-			    <span data-ng-class="{'warning': warning()}">remaining chars: {{remaining()}}</span>
-			    )
-		    </p>
-	    </div>
+            <h2 data-role="header" style="text-overflow:ellipsis; overflow:hidden; ">{{greeting}} "{{usrmsg}}"</h2>
+            You: <div class="practice-user-message"></div>
+            <p>
+                <span data-ng-show="remaining()">Keep Typing</span><span data-ng-show="(! remaining() )">Stop Typing</span> (
+                <span data-ng-class="{'warning': warning()}">remaining chars: {{remaining()}}</span>
+                )
+            </p>    
+        </div>
     </form>
     <asp:Literal ID="Lit1" runat="server"></asp:Literal>
 
--- Practice-AngularJS.aspx.cs
+++ Practice-AngularJS.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,16 +22,58 @@ namespace practice_angularjs {
 
         protected void Page_Init( object sender,EventArgs e ) {
             //Response.Write("Message from ASP.NET: Page_Init<br />\n");
+            Lit1.Text = "";
         }
 
         protected void Page_Load( object sender,EventArgs e ) {
             //Response.Write( "Message from ASP.NET: Page_Load<br />\n" );
-            Lit1.Text = "<em>" +
-                DateTime.Now.ToLongTimeString() + " - " + callBack(Context) + "</em>";
         }
 
         protected void Page_PreRender( object sender,EventArgs e ) {
-            //Response.Write("Message from ASP.NET: Page_PreRender<br />\n");
+			//Response.Write("Message from ASP.NET: Page_PreRender<br />\n");
+			Lit1.Text = Lit1.Text + "\n" +
+				"	<em>" + DateTime.Now.ToLongTimeString() + " - " +
+				callBack(Context) + "</em><br />";
+			selectData(Lit1,
+				"SELECT TOP 100 * FROM Person.Person"
+			);
         }
+
+		private void selectData( Literal lit, string selectCommandText ) {
+			try {
+				/* Change the connection string to match with your system. */
+				string selectConnection = @"Data Source=LIVE-PORTABLE\SQLEXPRESS;" +
+										@"Initial Catalog=AdventureWorks;" +
+										@"Integrated Security=True;"; //+
+										//@"Connect Timeout=30;" +
+										//@"User Instance=True;";
+				
+				SqlDataAdapter dataAdapter = new SqlDataAdapter(selectCommandText,selectConnection);
+				DataTable personData = new DataTable();
+				dataAdapter.Fill(personData);
+
+				for( int r = 0; r < personData.Rows.Count; r++ ) {
+					lit.Text = lit.Text +"\n"+
+						"	<span id="+'"';
+					for ( int f = 4;
+						f < 7; //personData.Rows[r].ItemArray.Length; 
+						f++
+					) {
+						lit.Text = lit.Text + personData.Rows[r].ItemArray[f];
+						if ( f < 6 ) lit.Text = lit.Text + '-';
+						else lit.Text = lit.Text + '"' + ">";
+					}
+					for( int f = 4; 
+						f < 7; //personData.Rows[r].ItemArray.Length; 
+						f++ 
+					){
+						lit.Text = lit.Text +" "+ personData.Rows[r].ItemArray[f];
+					}
+					lit.Text = lit.Text +"</span><br />";
+				}
+			} catch ( Exception e ) {
+				lit.Text = lit.Text + "\n" + e.Message;
+			}
+		}
     }
 }
